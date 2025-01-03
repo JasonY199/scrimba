38ca13c2a22a844b0bb68726c679dfc72aa2d2fb
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-03 17:21:23 -0500

    Dynamically displays cart items

--- restaurant-menu/script.js
+++ restaurant-menu/script.js
@@ -3,6 +3,8 @@ import menuArray from "./data.js";
 const menuWrapper = document.getElementById("menu-wrapper");
 const cartSection = document.getElementById("cart-section");
 const cartWrapper = document.getElementById("cart-wrapper");
+const cartItems = document.getElementById("cart-items");
+const totalPrice = document.getElementById("total-price");
 const pageDivider = `<div class="border-bottom"></div>`;
 const cart = [];
 
@@ -19,14 +21,14 @@ menuWrapper.innerHTML = menuArray
           </p>
           <h3 class="item-price">$${menuItem.price}.00</h3>
         </div>
-        <button class="item-add" data-id="${menuItem.id}">+</button>
+        <button class="item-add" data-add-id="${menuItem.id}">+</button>
       </div>`;
   })
   .join(pageDivider);
 
 document.addEventListener("click", (event) => {
-  if (event.target.dataset.id) {
-    addToCart(event.target.dataset.id);
+  if (event.target.dataset.addId) {
+    addToCart(event.target.dataset.addId);
   }
 });
 
@@ -50,9 +52,36 @@ const addToCart = (id) => {
 };
 
 const renderCart = () => {
+  if (cart.length === 0) {
+    if (!cartSection.classList.contains("hidden")) {
+      cartSection.classList.add("hidden");
+    }
+    return;
+  }
+
   if (cartSection.classList.contains("hidden")) {
     cartSection.classList.remove("hidden");
   }
 
-  // TODO continue here
+  cartItems.innerHTML = cart
+    .map((cartItem) => {
+      return `
+      <div class="cart-item">
+        <h3 class="cart-item-name">${cartItem.name}</h3>
+        <p class="cart-item-remove">
+          <a href="#" id="remove-cart-item" data-remove-id="${
+            cartItem.id
+          }">remove</a>
+        </p>
+        <p class="cart-item-unit-price">$${cartItem.price} ea</p>
+        <p class="cart-item-quantity">x ${cartItem.quantity}</p>
+        <p class="cart-item-subtotal">$${cartItem.price * cartItem.quantity}</p>
+      </div>`;
+    })
+    .join("");
+
+  const total = cart.reduce((total, current) => {
+    return total + current.price * current.quantity;
+  }, 0);
+  totalPrice.innerText = `$${total}.00`;
 };
