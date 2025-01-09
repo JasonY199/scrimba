279879668bfe66fc52cacb63fd04ddbea5161945
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-08 19:03:58 -0500

    Change hero to ID from class

--- learning-journal/scripts/global.js
+++ learning-journal/scripts/global.js
@@ -1,6 +1,7 @@
 import { loadContent } from "./dynamicLoader.js";
+import { BlogPosts } from "./data.js";
 
+// Load the main content dynamically
 document.addEventListener("DOMContentLoaded", () => {
-  // Load the main content dynamically
   loadContent("#main-content");
 });
