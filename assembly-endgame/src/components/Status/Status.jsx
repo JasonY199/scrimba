f4eeb12e51ba42dd153a83e28eb0481a31c70a4f
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-18 23:44:14 -0500

    Add status colors and flex layout for sizing

--- assembly-endgame/src/components/Status/Status.jsx
+++ assembly-endgame/src/components/Status/Status.jsx
@@ -2,7 +2,7 @@ import "./Status.css";
 
 function Status() {
   return (
-    <section className="status">
+    <section className="status win">
       <h2>You win!</h2>
       <p>Well done! 🎉</p>
     </section>
