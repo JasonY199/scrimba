36b16f926288dbe679401d4d9583927a3091e5d5
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-19 15:42:17 -0500

    Import and start using PropTypes

--- assembly-endgame/src/components/WordDisplay/WordDisplay.jsx
+++ assembly-endgame/src/components/WordDisplay/WordDisplay.jsx
@@ -1,7 +1,12 @@
+import PropTypes from "prop-types";
 import "./WordDisplay.css";
 
 function WordDisplay(props) {
   return <section>{props.currentWord}</section>;
 }
+
+WordDisplay.propTypes = {
+  currentWord: PropTypes.string.isRequired,
+};
 
 export default WordDisplay;
