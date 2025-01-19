f28fce800afe66acd0a0bb258d6056c9371307fa
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-19 15:09:10 -0500

    Refactor language chips component

--- assembly-endgame/src/components/LanguageChips/LanguageChips.jsx
+++ assembly-endgame/src/components/LanguageChips/LanguageChips.jsx
@@ -2,18 +2,19 @@ import "./LanguageChips.css";
 import language from "../../data/languages.js";
 
 function LanguageChips() {
-  function renderChips() {
-    return language.map((chip) => (
-      <div
-        key={chip.name}
-        style={{ color: chip.color, backgroundColor: chip.backgroundColor }}
-      >
-        {chip.name}
-      </div>
-    ));
-  }
+  const languageElements = language.map((lang) => {
+    const styles = {
+      color: lang.color,
+      backgroundColor: lang.backgroundColor,
+    };
+    return (
+      <span key={lang.name} style={styles}>
+        {lang.name}
+      </span>
+    );
+  });
 
-  return <section className="language-chips">{renderChips()}</section>;
+  return <section className="language-chips">{languageElements}</section>;
 }
 
 export default LanguageChips;
