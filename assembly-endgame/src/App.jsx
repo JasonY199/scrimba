27989ab4802b3638fb952dec0ae7207e75b5cd48
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-19 15:35:41 -0500

    Start creating WordDisplay component

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -1,13 +1,18 @@
+import React from "react";
 import Header from "./components/Header/Header";
 import Status from "./components/Status/Status";
 import LanguageChips from "./components/LanguageChips/LanguageChips";
+import WordDisplay from "./components/WordDisplay/WordDisplay";
 
 function App() {
+  const [currentWord] = React.useState("React");
+
   return (
     <>
       <Header />
       <Status />
       <LanguageChips />
+      <WordDisplay currentWord={currentWord} />
     </>
   );
 }
---
+++ assembly-endgame/src/components/WordDisplay/WordDisplay.jsx
@@ -0,0 +1,7 @@
+import "./WordDisplay.css";
+
+function WordDisplay(props) {
+  return <section>{props.currentWord}</section>;
+}
+
+export default WordDisplay;
