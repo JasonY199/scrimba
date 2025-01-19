83df6c6afae9dc62ebeafa93c4a0d80416f4830d
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-18 23:33:17 -0500

    Change flex from body to root, Create Status component

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Status from "./components/Status/Status";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Status />
+    </>
+  );
 }
 
 export default App;
---
+++ assembly-endgame/src/components/Status/Status.jsx
@@ -0,0 +1,12 @@
+import "./Status.css";
+
+function Status() {
+  return (
+    <section className="status">
+      <h2>You win!</h2>
+      <p>Well done! 🎉</p>
+    </section>
+  );
+}
+
+export default Status;
