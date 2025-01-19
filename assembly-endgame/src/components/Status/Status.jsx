0149c093205c91b97213b0a24aaf44e294298148
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-19 13:46:20 -0500

    Rename class status to game-status

--- assembly-endgame/src/components/Status/Status.jsx
+++ assembly-endgame/src/components/Status/Status.jsx
@@ -2,7 +2,7 @@ import "./Status.css";
 
 function Status() {
   return (
-    <section className="status win">
+    <section className="game-status win">
       <h2>You win!</h2>
       <p>Well done! 🎉</p>
     </section>
