d731e872c14f7054bf0b1c22350f8cf12a8e3dba
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-19 16:12:35 -0500

    Refactor to use split instead of array spread of the current word

--- assembly-endgame/src/components/WordDisplay/WordDisplay.jsx
+++ assembly-endgame/src/components/WordDisplay/WordDisplay.jsx
@@ -2,11 +2,9 @@ import PropTypes from "prop-types";
 import "./WordDisplay.css";
 
 function WordDisplay(props) {
-  const wordArr = [...props.currentWord];
-
-  const letterElements = wordArr.map((letter, index) => (
-    <span key={index}>{letter.toUpperCase()}</span>
-  ));
+  const letterElements = props.currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
 
   return <section className="current-word">{letterElements}</section>;
 }
