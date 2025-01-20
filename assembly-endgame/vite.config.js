a30b107298294376f25746143184117ceb0b60bf
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-01-19 20:02:42 -0500

    Allow running on mobile phone for testing, adjust page padding to look better on phone

--- assembly-endgame/vite.config.js
+++ assembly-endgame/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allows access from your phone using your Mac's IP address
+    port: 5173, // Optional: Ensures it runs on port 5173
+  },
+});
